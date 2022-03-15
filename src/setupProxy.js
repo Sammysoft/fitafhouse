eb6c36e1480b2adbc75ff79f0171ce9b50b55083
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2022-03-15 13:20:29 +0000

    made config setup for proxy to the backend endpoint

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8089/',
+      target: 'https://fitafhouse-api.herokuapp.com/',
       changeOrigin: true,
     })
   );
