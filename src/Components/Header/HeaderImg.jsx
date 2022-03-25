1b286f1a78ac776b4c441bd7e2ee9739bf3458d0
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2022-03-25 10:41:33 +0000

    a little adjustment on the landing page

--- src/Components/Header/HeaderImg.jsx
+++ src/Components/Header/HeaderImg.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeaderImg = () =>{
     return(
         <>
-            <div data-aos="fade-left" data-aos-duration="3000" className="header-img" style={{flex: "1"}} >
+            <div data-aos="fade-left" data-aos-duration="3000" className="header-img" style={{flex: "1", marginTop: '10vh'}} >
                             <img src="/images/head-img.jpg" width="100%" height="60%" alt="High quality House Cliparts For Free!" />
             </div>
         </>
