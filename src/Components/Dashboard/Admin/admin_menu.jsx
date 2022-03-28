c40e82214ff21e0eded0aee15bee82f024b76800
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2022-03-28 23:56:54 +0000

    fixed a little bug in API calls

--- src/Components/Dashboard/Admin/admin_menu.jsx
+++ src/Components/Dashboard/Admin/admin_menu.jsx
@@ -30,7 +30,7 @@ const AdminMenu = (props)=>{
 
 
     useEffect(()=>{
-                fetch('http://localhost:6069/api/stats')
+                fetch('https://fitafhouse-api.herokuapp.com/api/stats')
                 .then(async res=>{
                         const response = await res.json();
                         console.log(response)
