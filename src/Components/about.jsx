72af9f008900ce6b238e524598784b150b7a6a38
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2022-06-25 10:04:21 +0000

    making additional changes

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -9,25 +9,89 @@ const About = () => {
         className="about-wrapper"
       >
         <p>
-          FITAFHOUSE is a business house where we determine whether a project or
-          business or investment is viable or not. We raise money for qualified
-          ones and appoint credible team for execution.
+          <b>First International Trade and Finance House Ltd</b> also known as
+          FITAFHOUSE in business parlance opened her door to the public in
+          Nigeria on the 26th March, 1984. Similarly, the company opened her
+          door for patronage in Wales in United Kingdom on the 18th May, 1989
+          though very short-lived. The company started here in Benin City before
+          relocating to Akure, Ondo State in 1993.
+          <br />
+          Generally, Finance Companies (FC) in Nigeria witnessed crisis in the
+          early nineties because of lack of understanding of the operation of
+          the industry. For example, many finance company operators invested
+          short term fund placement for long term projects. Some also paid
+          upfront interest for fund placement. Ninety percent of the finance
+          companies in Nigeria during this crisis went under. In spite of the
+          crisis First International Trade and Finance House Ltd survived it.
+          The brand continued to wax strong till today not because we are
+          perfect but because of the mercy of God and His grace.
+          <br />
+          In the early twenties another crisis came up through investing in
+          stock exchange in Nigeria, hundreds of companies somersaulted but
+          First International Trade and Finance House Ltd lost money but
+          overcame the troubled water by dint of hardwork and prayer. After this
+          crisis, most of our money had to be invested in landed properties. The
+          good thing at the end of the crisis was that we did not have any
+          customer that we were indebted to. <br />
+          <br />
+          The third crisis came in 2019 when COVID 19 kept almost every person
+          at home. In real term the economy was upside down and the situation
+          was devastating. First International Trade and Finance House Ltd
+          delibrately closed her door to the public and operate from closed
+          door. Business was really bad!. Finance Company has a bright future in
+          Nigeria if it is well managed and supported by friends and well
+          wishers. When we consider the operation of commercial banks in
+          Nigeria, we could come to term with reality that their operation is
+          too conservative and this does not encourage young entrepreneur which
+          of cours are the heartbeat of any economy. China is a reference point.
+          The microfinance bank that came into existence because of the pulic
+          cry did not solve any problem. This is because most of their managers
+          were at one time or the other official in the commercial bank. Some of
+          them are wounded officers in the bank who sees opportunity to be extra
+          careful in their duties when employed in a microfinance bank. The
+          attitude of some Nigerian towards borrowing is nothing to write home
+          about. Most of the banks lord over their custmers as if they are not
+          partners in progress. Again most of the policies of the banks are
+          straight jacket. None of the operators can use their initiative. But
+          then this approach was due to abuse of power by the bank officials in
+          the past.
           <br />
           <br />
-          <br />
-          International trade business involves large sum of money expecially
-          now that we have global challenges with the value of our currencies
-          It's on this note that we humbly ask for your investment in this
-          company. Your massive investment in this company will give us
-          opportunity to enjoy Economies of Scale. What that means is that the
-          cost of our operation will go down with corresponding increase in our
-          profit. That means more returns will be made available to all
-          investors at the end of the financial year
-          <br />
+          This is where finance company comes in. First International Trade and
+          Finance House Ltd package is very clear. For example, we decide
+          whether to finance or not from appraising report of a project,
+          investment etc. We raise money for the qualified ones through the
+          instrumentality of equity, debt and grant funding. We also go ahead to
+          appoint credible officer to suppervise the execution of the project,
+          investment etc. The good thing about this approach is that it is
+          beautiful and most of the time is error-free.
           <br />
           <br />
-          We are located at FITAFHOUSE, 6 Tiamiyu Aborisade Crescent, Beside
-          Assembly of God Church, G.R.A Ado Ekiti, Ekiti State
+          Our door is opened to all and sundries to test whether their
+          project/investment etc is viable or not . The assurance we have for
+          any of our clients is that once a project/investment is viable
+          according to FITAFHOUSE conditionalities funding of the project is
+          assured. Our association with our bank is very cordial. In fact, some
+          of the projects are vetted by them and we get assistance from them
+          when we knock at their door. This is the procedure of finance company
+          operations in most of the developed countries of the world
+          <br />
+          <br />
+          The future of our company is very bright because of our strategy of
+          doing our business. We are not chewing more than we can bite. For
+          example, we hire to appraise and execute. We also hire for marketing.
+          We have a minimal overhead and this has been our survival strategy.
+          <br />
+          <br />
+          Thank you for your rapt attention. Don't forget you have an option to
+          acquire our shares or place fund for a period of time. Call on us on
+          our various numbers or email us. You can also facilitate grant for us.
+          Don't forget we are there for you when you need our services
+          concerning financing of your viable projects. All we are saying is
+          that your investment is safe with us and returns on the investment si
+          assured. Glory be to God we have moved to our permanent head office at
+          Ado Ekiti, Ekiti State.<br/>
+          <b>Management</b>
         </p>
       </div>
     </>
